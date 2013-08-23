ce2ca4c47c8c39c942e353feaa194dac0ea76b78
larrymelia <larrymelia@gmail.com>
2013-08-23 03:31:51 -0700

    Fixed WITNESS panic in VM Bus driver, replaced MSR r/w calls with intrinsic functions

--- sys/dev/hyperv/vmbus/hv_vmbus_drv_freebsd.c
+++ sys/dev/hyperv/vmbus/hv_vmbus_drv_freebsd.c
@@ -68,6 +68,7 @@ static int vmbus_rid;
 struct resource *intr_res;
 static int vmbus_irq = VMBUS_IRQ;
 static int vmbus_inited;
+static hv_setup_args setup_args; /* only CPU 0 supported at this time */
 
 /**
  * @brief Software interrupt thread routine to handle channel messages from
@@ -117,7 +118,7 @@ vmbus_msg_swintr(void *dummy)
 			 * This will cause message queue rescan to possibly
 			 * deliver another msg from the hypervisor
 			 */
-			hv_vmbus_write_msr(HV_X64_MSR_EOM, 0);
+			wrmsr(HV_X64_MSR_EOM, 0);
 	    }
 	}
 }
@@ -418,15 +419,28 @@ vmbus_bus_init(void)
 	/**
 	 * Notify the hypervisor of our irq.
 	 */
+	setup_args.vector = vector;
+	for(int i=0; i < 2; i++) {
+		setup_args.pageBuffers[i] =
+				malloc(PAGE_SIZE, M_DEVBUF, M_NOWAIT | M_ZERO);
+		if (setup_args.pageBuffers[i] == NULL) {
+			KASSERT(setup_args.pageBuffers[i] != NULL,
+					("Error VMBUS: malloc failed!"));
+			if (i > 0)
+				free(setup_args.pageBuffers[0], M_DEVBUF);
+			goto cleanup4;
+		}
+	}
 
-	smp_rendezvous(NULL, hv_vmbus_synic_init, NULL, &vector);
+	/* only CPU #0 supported at this time */
+	smp_rendezvous(NULL, hv_vmbus_synic_init, NULL, &setup_args);
 
-	/**
+	/*
 	 * Connect to VMBus in the root partition
 	 */
 	ret = hv_vmbus_connect();
 
-	if (ret)
+	if (ret != 0)
 	    goto cleanup4;
 
 	hv_vmbus_request_channel_offers();
@@ -440,7 +454,6 @@ vmbus_bus_init(void)
 	bus_teardown_intr(vmbus_devp, intr_res, vmbus_cookiep);
 
 	cleanup3:
-
 	bus_release_resource(vmbus_devp, SYS_RES_IRQ, vmbus_rid, intr_res);
 
 	cleanup2:
@@ -494,6 +507,11 @@ vmbus_bus_exit(void)
 	hv_vmbus_disconnect();
 
 	smp_rendezvous(NULL, hv_vmbus_synic_cleanup, NULL, NULL);
+
+	for(int i=0; i < 2; i++) {
+		if(setup_args.pageBuffers[i] != 0)
+			free(setup_args.pageBuffers[i], M_DEVBUF);
+	}
 
 	hv_vmbus_cleanup();
 
